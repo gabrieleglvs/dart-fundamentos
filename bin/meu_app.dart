0a94754930dac0c1ae4195abdf0790ed3c885f8f
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2026-06-16 14:03:07 -0300

    tipos primitivos e compostos

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,7 +1,24 @@
 import 'dart:io';
 
 void main() {
-  print("Olá, me chamo Dart. Qual é o seu nome?");
-  var nome = stdin.readLineSync();
-  print("Olá, $nome! Prazer em te conhecer!");
+  // Variáveis primitivas
+  int numeroInteiro = 10;
+  double numeroDecimal = 3.14;
+  String texto = "Este é um texto.";
+  bool isAtivo = true;
+
+  // Variáveis compostas
+  List<String> nomes = ["Ana", "João", "Maria"];
+  Map<String, String> agenda = {"Ana": "1234-5678", "João": "9876-5432"};
+  Set<int> numerosUnicos = {1, 2, 3, 4, 3}; // O número 3 só aparece uma vez
+
+  // Acessando valores
+  print("Número inteiro: $numeroInteiro");
+  print("Número decimal: $numeroDecimal");
+  print("Texto: $texto");
+  print("Ativo: $isAtivo");
+
+  print("Primeiro nome da lista: ${nomes[0]}");
+  print("Telefone da Ana: ${agenda['Ana']}");
+  print("Números únicos no conjunto: $numerosUnicos");
 }
