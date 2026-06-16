118725c9453cf0a9e8c64d61b653cd15464aa124
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2026-06-16 12:48:52 -0300

    comandos de entrada e saida

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 void main() {
-  print("Olá, Mundo!");
+  print("Olá, me chamo Dart. Qual é o seu nome?");
+  var nome = stdin.readLineSync();
+  print("Olá, $nome! Prazer em te conhecer!");
 }
